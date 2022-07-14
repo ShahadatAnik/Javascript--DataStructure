66b2bf641abab28d6d4a45e6296da80ef21aa49c
Shahadat Anik <git.shahadat.anik@gmail.com>
2022-07-14 21:23:05 +0600

    js start

---
+++ array.js
@@ -0,0 +1,28 @@
+
+const cars = ['BMW', 'Audi', 'Mercedes'];
+
+cars.push('Ferrari'); // add new element to the end of the array
+cars.unshift('Ferrari'); // add to the beginning
+cars.pop(); // remove last element
+cars.shift(); // remove first element
+
+for (const car of cars){
+    console.log(car);
+}
+
+
+/*
+# Array Big-O time complexity
+
+Insert/remove from end - O(1)
+Insert/remove from beginning - O(n)
+
+Access- - 0(1)
+Search - O(n)
+Push/pop - O(1)
+
+Shift/unshift/concat / slice / splice - O(n)
+
+forEach/map/filter/reduce - O(n)
+
+*/
---
+++ object.js
@@ -0,0 +1,30 @@
+const obj = {
+    name: 'John',
+    age: 30,
+    isMarried: false,
+    sayInfo: function() {
+        console.log("Name: "+ this.name);
+        console.log("Age: "+ this.age);
+        console.log("Married: "+ this.isMarried);
+        
+    }
+}
+
+obj.sex = "M" // add in object
+
+console.log(obj);
+obj.sayInfo();
+
+/*
+Object - Big-O time complexity
+
+Insert - O(1)
+Remove - O(1)
+Access - O(1)
+
+Search - O(n)
+
+Object.keys()- O(n)
+Object.values() - O(n)
+Object.entries() - O(n)
+*/
